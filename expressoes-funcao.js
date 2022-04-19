0be1e0c73f9d2a2d04ede53a401598c0d0aa64f7
Bruno Vasconcelos <bvasconcelos710@gmail.com>
2022-04-19 16:01:44 -0300

    funções

---
+++ expressoes-funcao.js
@@ -0,0 +1,3 @@
+// expressões de função
+const soma = function (num1, num2) { return num1 + num2 }
+console.log(soma(5, 5));
---
+++ funcoes.js
@@ -0,0 +1,12 @@
+function imprimeTexto(texto) {
+    console.log(texto);
+}
+
+imprimeTexto("Olá, mundo!");
+
+
+function soma() {
+    return 2 + 2;
+}
+
+console.log(soma());
