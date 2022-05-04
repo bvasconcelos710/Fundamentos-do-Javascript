c7a34c7a553b638bde07aca59804cc74f5e4dfbc
Bruno Vasconcelos <bvasconcelos710@gmail.com>
2022-05-04 15:01:04 -0300

    prototipos

---
+++ cliente poupança.js
@@ -0,0 +1,27 @@
+function Cliente(nome, cpf, email, saldo) {
+    this.nome = nome;
+    this.cpf = cpf;
+    this.email = email;
+    this.salddo = saldo;
+    this.depositar = function (valor) {
+        this.saldo += valor;
+    }
+
+
+}
+
+function clientePoupanca(nome, cpf, email, saldo, saldoPoup) {
+    Cliente.call(this.nome, cpf, email, saldo);
+    this.saldoPoup = saldoPoup;
+
+}
+const ju = new clientePoupanca("Ju", "12345898411", "ju@gagag", 100, 200);
+
+console.log(ju);
+
+clientePoupanca.prototype.depositarPoup = function (valor) {
+    this.saldoPoup += valor
+}
+
+ju.depositarPoup(30)
+console.log(ju.saldoPoup)
